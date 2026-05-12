7ec0c1d71f3e3f7b115fab32ba5c0bce83e0ff3f
Rajveer Pathak <tarupa123@gmail.com>
2026-05-12 11:46:34 +0530

    fixed base url for backend

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://campus-event-management-system-lhpe.onrender.com/api/v1",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
